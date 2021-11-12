8a85a1e382faabb7389e16f553b6fbce50f01697
Sam Okpara <samuelopra@gmail.com>
2021-11-12 18:18:53 -0500

    initial shitty youtube commit with searchbar

---
+++ src/components/SearchBar.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const SearchBar = () => {
+  return (
+    <div className="search-bar ui segment">
+      <form className="ui form">
+        <div className="field">
+          <label>Shitty video player</label>
+          <input type="text" />
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default SearchBar;
